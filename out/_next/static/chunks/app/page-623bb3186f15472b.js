db18158f769a22c91c221c38db26cf2a56728d5e
Tianpai Z. <tianpai.zhang@mail.utoronto.ca>
2024-10-18 16:40:12 -0400

    FE!N FE!N FE!N FE!N FE!N FE!N FE!N FE!N

--- out/_next/static/chunks/app/page-623bb3186f15472b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{4391:function(n,e,u){Promise.resolve().then(u.t.bind(u,7395,23))},7395:function(){}},function(n){n.O(0,[864,971,23,744],function(){return n(n.s=4391)}),_N_E=n.O()}]);
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,28 @@ import "katex/dist/katex.min.css";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { dark } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
+function randomIntFromInterval(min: number, max: number) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 export default function Home() {
-  return (
+  let seg = (
     <a href="https://en.wikipedia.org/wiki/Segmentation_Fault">
-      {/* <Latex>$G=(V,E)$</Latex> */}
       <SyntaxHighlighter language="bash" style={dark}>
         Segmentation Fault!
       </SyntaxHighlighter>
     </a>
   );
+
+  let fein = (
+    <a href="https://www.youtube.com/watch?v=B9synWjqBn8">
+      <SyntaxHighlighter language="text" style={dark}>
+        FE!N
+      </SyntaxHighlighter>
+    </a>
+  );
+  const home = [seg, fein];
+  let random = randomIntFromInterval(0, home.length - 1);
+  return home[random];
 }
